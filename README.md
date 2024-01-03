8dc6450e3a1d467d4719482bd00ca4577d53ed55
fatarick <61841951+fatarick@users.noreply.github.com>
2024-01-03 21:11:08 +0200

    gui file
    Added gui requirements command.
    Added empty GUI File
    Removed and added some commands

--- README.md
+++ README.md
@@ -1,12 +1,9 @@
-# About this branch
-DEV - experiment features and sometimes unstable. When the code is stable it will be released in the main branch and sometimes some small features from DEV Branch will be released as small update for the main branch
-
 # How to start?
-For Windows:
-Install Python to System Folder
-Open The Command Prompt and write:
+just write this command:
+For windows:
 python dos.py
-For UNIX-Based:
-For macOS Users install Python earlier, For Linux or FreeBSD Users the Python is PreInstalled already
-Open Terminal and write this command
-python3 dos.py
+For linux and macOS:
+python3 dosun.py
+
+# What file good for me?
+For Windows users dos.py, For unix-based dosun.py
--- dos.py
+++ dos.py
@@ -6,7 +6,7 @@ from datetime import datetime
 os.system('cls')
 print("Welcome to ")
 print("FatCisDos Dev")
-print("Build 1211")
+print("Build 1212")
 time.sleep(2)
 now = datetime.now()
 
@@ -24,7 +24,7 @@ while True:
         print("Test command")
     if com1 == "ver":
         print("FatCisDOS Dev")
-        print("Build 1210")
+        print("Build 1212")
         print("©FatCisDev.")
     if com1 == "exit":
         os.system('cls')
@@ -41,22 +41,20 @@ while True:
         print ("help - this command            dir - view files on this category")
         print ("reset - restart the dos    clear - clear all messeges on the dos")
         print ("test - test command         exit - exit the dos")
-        print ("ver - version            vk - FatCisDev VK Group")
+        print ("ver - version               time - shows the time")
         print ("info - DOS Information      authors - DOS Authors")
-        print ("color green,blue or gray - making DOS green, blue and gray!")
-        print ("echo - repeats words        time - shows the time "   )
+        print ("echo - repeats words         gui - Starts the GUI Variant (PRE-DEV)")
+        print ("guireq - GUI Requirments      pip - Installs python libraries in your choice")
     if com1 == "authors":
         print ("Nikita Rojdestvin - Versions until 8.0.1")
         print ("Artem Litvinsev - Versions from 8.1. FatCisDev Owner")
         print ("Seva Tretyakov - Former Coder. XDAFAD Software Owner")
     if com1 == "info":
-        print("FatCisDOS is a DOS without installing By FatCisDev (Former MECIS Software). you must be write in cmd or terminal python dos.py and it starts.")
+        print("FatCisDOS is a DOS without installing By FatCisDev (Former MECIS Software).")
 
-    if com1 == "kakish":
-        print("ugh. ugh. ugh.")
     if com1 == "echo":
         word=input("Write your words:")
-        print(echo)
+        print(word)
     if com1 == "command":
         com = input("Which command are you need? ")
         os.system(com)
@@ -65,8 +63,16 @@ while True:
 
         current_time = now.strftime("%H:%M")
         print("Current Time: ", current_time)
-    if com1 == "fatcis":
-        print("https://vk.com/fatcisdev")
+
+    if com1 == "gui":
+        os.system("python3 gui.py")
+
+    if com1 == "guireq":
+        os.system("pip install pyqt6")
+    
+    if com1 == "pip":
+        libname = input("Enter library name: ")
+        os.system("pip install " + libname)
 
 
         
