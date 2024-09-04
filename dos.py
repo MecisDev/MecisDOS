78c81230acdcdd0a7106dae93e01bcc82c12808c
Artem Litvintsev <61841951+fatarick@users.noreply.github.com>
2024-09-04 18:39:53 +0300

    Update dos.py

--- dos.py
+++ dos.py
@@ -5,8 +5,8 @@ from datetime import datetime
 
 os.system('cls')
 print("Welcome to ")
-print("FatCisDos 13")
-print("Build 1235")
+print("MecisDOS 13")
+print("Build 1240")
 time.sleep(2)
 now = datetime.now()
 
@@ -17,16 +17,16 @@ time.sleep(0.2)
 print("One little bit...")
 time.sleep(1)
 os.system('cls')
-print("Welcome to FatCisDOS DEV! This is a OS emulator written on python.")
+print("Welcome to MecisDOS 13! This is a OS emulator written on python.")
 while True:
-    com1 = input("root@fatcisdos:>/ ")
+    com1 = input("root@mecisdos:>/ ")
     if com1 == "test":
         print("Test command")
     if com1 == "ver":
-        print("FatCisDOS 13")
+        print("MecisDOS 13")
         print("Build 1235")
 
-        print("©FatCisDev.")
+        print("©MECIS.")
     elif com1 == "exit":
         os.system('cls')
         break
@@ -48,19 +48,13 @@ while True:
         print ("github - shows the link to the project's github repository")
     elif com1 == "authors":
         print ("Nikita Rojdestvin - Versions until 8.0.1")
-        print ("Artem Litvinsev - Versions from 8.1. FatCisDev Owner")
+        print ("Artem Litvinsev - Versions from 8.1. MECIS Dev Owner")
         print ("Seva Tretyakov - Former Coder. XDAFAD Software Owner")
     elif com1 == "info":
-        print("FatCisDOS is a DOS without installing By FatCisDev (Former MECIS Software). you must be write in cmd or terminal python dos.py and it starts.")
+        print("MecisDOS is a DOS without installing By MECIS Dev. you must be write in cmd or terminal python dos.py and it starts.")
 
     elif com1 == "kakish":
         print("ugh. ugh. ugh.")
-    elif com1 == "color green":
-        os.system("color 2")
-    elif com1 == "color blue":
-        os.system("color 1")
-    elif com1 == "color gray":
-        os.system("color 8")
     elif com1 == "echo":
         word = input("what word do you need?: ")
         os.system("echo " +word)
@@ -90,7 +84,7 @@ while True:
       keyboard.wait('ctrl+x')
 
     elif com1 == "github":
-        print("https://github.com/FatCisDev/FatCisDos/")
+        print("https://github.com/MecisDev/MecisDOS/")
 
     else:
         print("The Command is Unknown! Try again or write help.")
